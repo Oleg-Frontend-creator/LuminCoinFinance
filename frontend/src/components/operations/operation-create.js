e761a83e1e1e89713dadf3b0197f57df370122a1
Oleg <oleggrom12@gmail.com>
2025-12-05 21:48:43 +0300

    Building fullstack SPA on JS

---
+++ frontend/src/components/operations/operation-create.js
@@ -0,0 +1,114 @@
+import {ClickUtils} from "../../utils/click-utils";
+import {HttpUtils} from "../../utils/http-utils";
+import {UrlUtils} from "../../utils/url-utils";
+
+export class OperationCreate {
+    constructor(openNewRoute) {
+        this.operationType = UrlUtils.getUrlParam('type');
+        this.openNewRoute = openNewRoute;
+        if (!this.operationType) {
+            return this.openNewRoute('/');
+        }
+        ClickUtils.addEvents(window.location.pathname);
+        this.findElements().then();
+
+        if (this.operationType === 'expense') {
+            this.typeInputElement.value = "Расход";
+        } else if (this.operationType === 'income') {
+            this.typeInputElement.value = "Доход";
+        }
+    }
+
+    async findElements() {
+        this.typeInputElement = document.getElementById('typeInput');
+        this.categorySelectElement = document.getElementById('categorySelect');
+        this.createButtonElement = document.getElementById('create-button');
+        await this.setCategoriesInSelect();
+        this.amountInputElement = document.getElementById('amountInput');
+        this.dateInputElement = document.getElementById('dateInput');
+        this.commentInputElement = document.getElementById('commentInput');
+    }
+
+    async createNewOperation(data) {
+        const result = await HttpUtils.request('/operations', 'POST', true, data);
+
+        if (result.error) {
+            console.log(result.response.message);
+            return result.redirect ? this.openNewRoute(result.redirect) : null;
+        }
+
+        return this.openNewRoute('/operations');
+    }
+
+    async createButtonClick(e) {
+        if (this.validation()) {
+            e.preventDefault();
+
+            const data = {
+                type: this.operationType,
+                amount: this.amountInputElement.value,
+                comment: this.commentInputElement.value,
+                date: this.dateInputElement.value,
+                category_id: parseInt(this.categorySelectElement.value)
+            };
+
+            return this.createNewOperation(data);
+        }
+    }
+
+    validation() {
+        let isError = false;
+
+        if (parseInt(this.categorySelectElement.value) === -1) {
+            this.categorySelectElement.classList.add('is-invalid');
+            isError = true;
+        } else {
+            this.categorySelectElement.classList.remove('is-invalid');
+        }
+
+        if (!this.amountInputElement.value || !this.amountInputElement.value.match(/^\d+\$?$/)) {
+            this.amountInputElement.classList.add('is-invalid');
+            isError = true;
+        } else {
+            this.amountInputElement.classList.remove('is-invalid');
+        }
+
+        if (!this.dateInputElement.value) {
+            this.dateInputElement.classList.add('is-invalid');
+            isError = true;
+        } else {
+            this.dateInputElement.classList.remove('is-invalid');
+        }
+
+        if (!this.commentInputElement.value) {
+            this.commentInputElement.classList.add('is-invalid');
+            isError = true;
+        } else {
+            this.commentInputElement.classList.remove('is-invalid');
+        }
+
+        return !isError;
+    }
+
+    async setCategoriesInSelect() {
+        this.categories = await HttpUtils.request('/categories/' + this.operationType);
+        if (this.categories.error) {
+            console.log(this.categories.response ? this.categories.response.message : 'неизвестная ошибка');
+            return this.categories.redirect ? this.openNewRoute(this.categories.redirect) : null;
+        }
+
+        if (this.categories.response && this.categories.response.length) {
+            for (let i = 0; i < this.categories.response.length; i++) {
+                const optionElement = document.createElement('option');
+                optionElement.value = this.categories.response[i].id;
+                optionElement.innerText = this.categories.response[i].title;
+                this.categorySelectElement.appendChild(optionElement);
+            }
+        } else {
+            alert('Категории отсутствуют. Пожалуйста, вернитесь на страницы создания категорий и создайте их.');
+            return this.openNewRoute('/');
+        }
+
+        this.createButtonElement.addEventListener('click', this.createButtonClick.bind(this));
+    }
+}
